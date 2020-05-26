2ce78b56ed77582294e20c5b33458c5cbde4861d
GinaGrg1 <gina524286@gmail.com>
2020-05-26 17:02:32 +0000

    update image field

--- personal_portfolio/portfolio/models.py
+++ personal_portfolio/portfolio/models.py
@@ -4,9 +4,8 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    image = models.ImageField()  # If blank, it will load to portfolio/images
-    url = models.URLField()
+    image = models.ImageField(upload_to='portfolio/images')  # If blank, it will load to portfolio/images
+    url = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
-    
